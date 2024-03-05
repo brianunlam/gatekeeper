dd899714cf79831b8a9f8f0462cc70ddf5894bd6
brianunlam <brian.alonso.a@gmail.com>
2024-03-05 20:06:09 -0300

    Try with ugly class

--- src/main.ts
+++ src/main.ts
@@ -1,120 +1,106 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-class JobStatus {
-  totalJobs: string[] = []
-  completeJobs: string[] = []
-  errJobs: string[] = []
-  ignoredJobs: string[] = []
-
-  constructor(
-    totalJobs: string[],
-    completeJobs: string[],
-    errJobs: string[],
-    ignoredJobs: string[]
-  ) {
-    this.totalJobs = totalJobs
-    this.completeJobs = completeJobs
-    this.errJobs = errJobs
-    this.ignoredJobs = ignoredJobs
-  }
-
-  isSuccess(): boolean {
-    return (
-      this.errJobs.length === 0 &&
-      this.completeJobs.length === this.totalJobs.length
-    )
-  }
-
-  detail(): string {
-    const incompleteJobs = this.getIncompleteJobs()
-    let result = `${this.completeJobs.length} out of ${this.totalJobs.length}
-Total job count:       ${this.totalJobs.length}
-Completed job count:   ${this.completeJobs.length}
-Incompleted job count: ${incompleteJobs.length}
-Failed job count:      ${this.errJobs.length}
-Ignored job count:     ${this.ignoredJobs.length}`
-
-    result += `
-::group::Failed jobs
-${this.prettyPrintJobList(this.errJobs)}
-::endgroup::
-::group::Completed jobs
-${this.prettyPrintJobList(this.completeJobs)}
-::endgroup::
-::group::Incomplete jobs
-${this.prettyPrintJobList(incompleteJobs)}
-::endgroup::
-::group::Ignored jobs
-${this.prettyPrintJobList(this.ignoredJobs)}
-::endgroup::
-::group::All jobs
-${this.prettyPrintJobList(this.totalJobs)}
-::endgroup::`
-
-    return result
-  }
-
-  private prettyPrintJobList(jobs: string[]): string {
-    return jobs.length === 0 ? '[]' : jobs.map(job => `- ${job}`).join('\n')
-  }
-
-  private getIncompleteJobs(): string[] {
-    return this.totalJobs.filter(
-      job =>
-        !this.completeJobs.includes(job) &&
-        !this.errJobs.includes(job) &&
-        !this.ignoredJobs.includes(job)
-    )
-  }
-}
-
 async function getJobStatuses(
   octokit: ReturnType<typeof github.getOctokit>,
   owner: string,
-  repo: string
-): Promise<JobStatus> {
-  const currentRunId = process.env['GITHUB_RUN_ID']
+  repo: string,
+  context: typeof github.context = github.context
+): Promise<void> {
+  const ref = context.payload.pull_request?.head.sha
+  const check_runs = (
+    await octokit.rest.checks.listForRef({
+      owner,
+      repo,
+      ref
+    })
+  ).data.check_runs
 
-  if (!currentRunId) {
-    throw new Error('GITHUB_RUN_ID environment variable is not set.')
-  }
+  core.info(`Check runs: ${JSON.stringify(check_runs, null, 2)}`)
 
-  // Log the current run ID
-  core.info(`Current Workflow Run ID: ${currentRunId}`)
-
-  // Fetch and log the jobs for the current workflow run
-  const jobs = await octokit.rest.actions.listJobsForWorkflowRun({
-    owner,
-    repo,
-    run_id: parseInt(currentRunId, 10)
-  })
-
-  for (const job of jobs.data.jobs) {
+  for (const ran of check_runs) {
     core.info(
-      `Job: ${job.name}, Status: ${job.status}, Conclusion: ${job.conclusion}`
+      `Check run: ${ran.app?.slug}, ID: ${ran.id}, currentJobId: ${context.runId}, ghjob: ${context.job}`
     )
+    if (ran.name === 'GitHub Actions Test') {
+      core.info(
+        `Check run: ${JSON.stringify(ran, null, 2)}, context: ${JSON.stringify(context, null, 2)}`
+      )
+    }
+
+    if (ran.app?.slug === 'github-actions' && ran.id !== context.runId) {
+      // Get the corresponding Actions job.
+      // The Actions job ID is the same as the Checks run ID
+      // (not to be confused with the Actions run ID).
+      const job = (
+        await octokit.rest.actions.getJobForWorkflowRun({
+          owner,
+          repo,
+          job_id: ran.id
+        })
+      ).data
+
+      // Now, get the Actions run that this job is in.
+      const actions_run = (
+        await octokit.rest.actions.getWorkflowRun({
+          owner,
+          repo,
+          run_id: job.run_id
+        })
+      ).data
+      core.info(
+        `Job: ${job.name}, Status: ${job.status}, Conclusion: ${job.conclusion}`
+      )
+      core.info(`Actions Run: ${actions_run.id}, Event: ${actions_run.event}`)
+    }
   }
 
-  return new JobStatus([], [], [], [])
+  // const jobStatuses = new JobStatus([], [], [], [])
+
+  // // Log the current run ID
+  // core.info(`Current Workflow Run ID: ${currentRunId}`)
+
+  // // Fetch and log the jobs for the current workflow run
+  // const jobs = await octokit.rest.actions.listJobsForWorkflowRun({
+  //   owner,
+  //   repo,
+  //   run_id: parseInt(currentRunId, 10)
+  // })
+
+  // for (const job of jobs.data.jobs) {
+  //   core.info(
+  //     `Job: ${job.name}, Status: ${job.status}, Conclusion: ${job.conclusion}`
+  //   )
+  //   jobStatuses.totalJobs.push(job.name)
+  //   if (job.conclusion === 'success') {
+  //     jobStatuses.completeJobs.push(job.name)
+  //   } else if (job.conclusion !== 'skipped') {
+  //     // Assuming 'skipped' jobs are ignored
+  //     jobStatuses.errJobs.push(job.name)
+  //   }
+  // }
+
+  return
 }
 
 export async function run(): Promise<void> {
   try {
     const token = core.getInput('repo-token', { required: true })
-    const interval = parseInt(core.getInput('interval'), 10) || 60000
+    // const interval = parseInt(core.getInput('interval'), 10) || 60000
 
     const octokit = github.getOctokit(token)
     const { owner, repo } = github.context.repo
 
-    let jobStatuses = await getJobStatuses(octokit, owner, repo)
-    while (!jobStatuses.isSuccess()) {
-      core.info('Not all jobs are successful. Waiting...')
-      await new Promise<void>(resolve => setTimeout(resolve, interval))
-      jobStatuses = await getJobStatuses(octokit, owner, repo)
-    }
+    // let jobStatuses = await getJobStatuses(octokit, owner, repo)
+    core.info('About to run getJobStatuses')
+    await getJobStatuses(octokit, owner, repo)
+    // while (!jobStatuses.isSuccess()) {
+    //   core.info('Not all jobs are successful. Waiting...')
+    //   core.info(jobStatuses.detail())
+    //   await new Promise<void>(resolve => setTimeout(resolve, interval))
+    //   jobStatuses = await getJobStatuses(octokit, owner, repo)
+    // }
 
-    core.info(jobStatuses.detail())
     core.setOutput('status', 'success')
   } catch (error) {
     if (error instanceof Error) {
@@ -122,5 +108,3 @@ export async function run(): Promise<void> {
     }
   }
 }
-
-run()
